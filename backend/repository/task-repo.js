f21548989b71d38ed771dfc4fe3c272f94c0d8ee
Nikko <nikkonuriel@gmail.com>
2024-04-30 17:54:06 +0800

    new comments

--- backend/repository/task-repo.js
+++ backend/repository/task-repo.js
@@ -1,9 +1,11 @@
 const pool = require('./database');
 
-//ADD
+//ADD - Adds a new task to the database.
 const addTask = (title, description, callback) => {
     const sql = 'INSERT INTO tasks (title, description) VALUES (?, ?)';
     pool.query(sql, [title, description], (err, result) => {
+        // If an error occurs during the database operation, 
+        //log the error and call the callback function with the error.
         if (err) {
             console.error('Error adding task:', err);
             callback(err);
@@ -13,10 +15,12 @@ const addTask = (title, description, callback) => {
     });
 };
 
-//LIST
+//LIST - Retrieves all tasks from the database.
 const getTasks = (callback) => {
     const sql = 'SELECT * FROM tasks';
     pool.query(sql, (err, results) => {
+        // If an error occurs during the database operation, 
+        //log the error and call the callback function with the error.
         if (err) {
             console.error('Error fetching tasks:', err);
             callback(err);
@@ -26,10 +30,12 @@ const getTasks = (callback) => {
     });
 };
 
-//DELETE
+//DELETE - Deletes a task from the database.
 const deleteTask = (taskId, callback) => {
     const sql = 'DELETE FROM tasks WHERE id = ?';
     pool.query(sql, [taskId], (err, result) => {
+        // If an error occurs during the database operation, 
+        //log the error and call the callback function with the error.
         if (err) {
             console.error('Error deleting task:', err);
             callback(err);
@@ -39,10 +45,12 @@ const deleteTask = (taskId, callback) => {
     });
 };
 
-//EDIT
+//EDIT - Edits a task from the database.
 const updateTask = (taskId, title, description, status, callback) => {
     const sql = 'UPDATE tasks SET title = ?, description = ?, status = ? WHERE id = ?';
     pool.query(sql, [title, description, status, taskId], (err, result) => {
+        // If an error occurs during the database operation, 
+        //log the error and call the callback function with the error.
         if (err) {
             console.error('Error updating task:', err);
             callback(err);
